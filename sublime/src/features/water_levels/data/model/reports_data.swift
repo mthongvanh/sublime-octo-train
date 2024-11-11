01fb28a29d963a8f4255dac8641ec007e05ea1f7
Mike Thongvanh <michael@thongvanh.com>
2024-11-11 11:30:22 -0600

    feat: implemented water level report listing with swift ui

--- sublime/src/features/water_levels/data/model/reports_data.swift
+++ sublime/src/features/water_levels/data/model/reports_data.swift
@@ -6,3 +6,120 @@
 //
 
 import Foundation
+
+struct ReportSection: Identifiable {
+    var id: String {
+        get {
+            title
+        }
+    }
+    
+    var title: String
+    var data: [WaterLevelReport]
+    
+    init(title: String, data: [WaterLevelReport]) {
+        self.title = title
+        self.data = data
+    }
+}
+
+@Observable class ReportsData {
+    
+    /// user-facing water body reports split by
+    var displayedData = [ReportSection]()
+    
+    /// all favorited and non-favorited water body stations
+    private var reportCollection = [WaterLevelReport]()
+    
+    private var waterLevelRepository: WaterLevelRepository
+    
+    private var favoriteCodes = [String]()
+    
+    init(reports: [WaterLevelReport] = [WaterLevelReport](), waterLevelRepo: WaterLevelRepository) {
+        reportCollection = reports
+        waterLevelRepository = waterLevelRepo
+    }
+    
+    @MainActor
+    // @MainActor guarantees that updates happen on main thread
+    func loadData() async {
+        do {
+            _reportCollection = try await waterLevelRepository.getWaterLevels()
+            
+            self.favoriteCodes = try waterLevelRepository.getFavorites()
+            
+            let r = sortReports(reports: _reportCollection)
+            updateReports(favorites: r.favorites, others: r.other)
+        } catch {
+            debugPrint("error loading reports data \(error)")
+        }
+    }
+    
+    func updateReports(favorites: [WaterLevelReport] = [WaterLevelReport](), others: [WaterLevelReport] = [WaterLevelReport]()) {
+        var reportSections = [ReportSection]()
+        if (!favorites.isEmpty) {
+            reportSections.append(
+                ReportSection(title: "Favorite Rivers", data: favorites));
+        }
+        if (!others.isEmpty) {
+            reportSections.append(
+                ReportSection(title: "Rivers", data: others))
+        }
+        displayedData = reportSections
+    }
+    
+    
+    func isFavorite(stationCode: String) -> Bool {
+        do {
+            let favorite = try _waterLevelRepository.getFavoriteStatus(stationCode: stationCode)
+            return favorite
+        } catch {
+            print(error)
+        }
+        return false
+    }
+    
+    @MainActor
+    func didToggleFavorite(stationCode: String) async {
+        _ = await toggleFavorite(stationCode: stationCode)
+        let r = sortReports(reports: _reportCollection)
+        updateReports(favorites: r.favorites, others: r.other)
+    }
+    
+    @MainActor
+    func toggleFavorite(stationCode: String) async -> Bool {
+        do {
+            let fav = try await _waterLevelRepository.toggleStationFavorite(stationCode: stationCode)
+            if fav {
+                favoriteCodes.append(stationCode)
+            } else {
+                if let index = favoriteCodes.firstIndex(of: stationCode) {
+                    favoriteCodes.remove(at: index)
+                }
+            }
+            return fav
+        } catch {
+            debugPrint("error occurred while favoriting")
+        }
+        return false
+    }
+    
+    func fetchFavoriteStatus(stationCode: String) {
+        
+    }
+    
+    func sortReports(reports: [WaterLevelReport]) -> (favorites: [WaterLevelReport], other: [WaterLevelReport]) {
+        var favorite = [WaterLevelReport]()
+        var nonfavorite = [WaterLevelReport]()
+        for report in reports {
+            if favoriteCodes.contains(report.stationCode) {
+                favorite.append(report)
+            } else {
+                nonfavorite.append(report)
+            }
+        }
+        
+        return (favorites: favorite.sorted { $0.waterbody < $1.waterbody },
+                other: nonfavorite.sorted { $0.waterbody < $1.waterbody })
+    }
+}
--- sublime/src/features/water_levels/presentation/reports/reports_view.swift
+++ sublime/src/features/water_levels/presentation/reports/reports_view.swift
@@ -8,11 +8,57 @@
 import SwiftUI
 
 struct reports_view: View {
+    
+    private var reportsViewModel: ReportsViewModel?
+    @State private var reportsData: ReportsData
+    
+    init(reportsViewModel: ReportsViewModel?, reportsData: ReportsData) {
+        self.reportsViewModel = reportsViewModel
+        self.reportsData = reportsData
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            List {
+                ForEach(reportsData.displayedData) { section in
+                    Section(header: Text(section.title)) {
+                        ForEach(section.data) { report in
+                            HStack(content: {
+                                VStack(alignment: .leading, content: {
+                                    Text(
+                                        "\(report.waterbody) @ \(report.station)")
+                                    HStack(content: {
+                                        Text("flow: \(report.speed) m^3/s, depth: \(report.depth) cm").font(.subheadline)
+                                    })
+                                })
+                                Spacer()
+                                Button(action: {
+                                    Task {
+                                        await reportsData.didToggleFavorite(stationCode:report.stationCode)
+                                    }
+                                }, label: {
+                                    let favorite = reportsData.isFavorite(stationCode: report.stationCode)
+                                    Image(
+                                        systemName: favorite
+                                        ? "star.fill"
+                                        : "star"
+                                    ).tint(favorite ? Color.yellow : Color.gray)
+                                })
+                            })
+                        }
+                    }
+                }
+            }
+        }
+        .task {
+            await reportsData.loadData()
+        }
     }
 }
 
 #Preview {
-    reports_view()
+    let mockGetFavorites = MockGetFavorites(repo: MockWaterLevelRepo())
+    let reports = MockReportsViewModel(getFavoriteStatus: mockGetFavorites)
+    let reportsData = ReportsData(reports: reports.reports, waterLevelRepo: MockWaterLevelRepo())
+    return reports_view(reportsViewModel: reports, reportsData: reportsData)
 }
