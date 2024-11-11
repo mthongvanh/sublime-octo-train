209ed101b746f3f766f480fdf421d481b71d7187
Mike Thongvanh <michael@thongvanh.com>
2024-11-11 11:05:00 -0600

    feat: add support for favoriting water stations

---
+++ sublime/src/features/water_levels/data/data_source/water_level_local_data_source_impl.swift
@@ -0,0 +1,8 @@
+//
+//  water_level_local_data_source_impl.swift
+//  sublime
+//
+//  Created by Michael Thongvanh on 5/4/24.
+//
+
+import Foundation
---
+++ sublime/src/features/water_levels/presentation/reports/reports_view.swift
@@ -0,0 +1,18 @@
+//
+//  reports_view.swift
+//  sublime
+//
+//  Created by Michael Thongvanh on 11/8/24.
+//
+
+import SwiftUI
+
+struct reports_view: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    reports_view()
+}
